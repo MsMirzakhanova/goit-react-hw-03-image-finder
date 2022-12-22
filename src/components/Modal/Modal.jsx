376e361af2fc062eb182886b6c4f791624c2e8fd
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-12-22 23:50:48 +0200

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { createPortal } from "react-dom";
 import styles from "./Modal.module.css";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 
 const modalRoot = document.querySelector('#modal-root');
